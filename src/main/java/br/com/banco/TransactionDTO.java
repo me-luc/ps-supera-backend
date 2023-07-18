782fa5486463a8626371abd12559fbceda917ab7
me.luc <lukasalmeida372@gmail.com>
2023-07-17 23:09:57 -0300

    feat: update date format

--- src/main/java/br/com/banco/TransactionDTO.java
+++ src/main/java/br/com/banco/TransactionDTO.java
@@ -2,33 +2,46 @@ package br.com.banco;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class TransactionDTO {
     private Date beginDate;
     private Date endDate;
     private String operatorName;
+    private  SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    {
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
 
     public TransactionDTO(String beginDate, String endDate, String operatorName) throws ParseException {
-        this.beginDate = setBeginDate(beginDate);
-        this.endDate = setEndDate(endDate);
+        setBeginDate(beginDate);
+        setEndDate(endDate);
         this.operatorName = operatorName;
     }
 
-    public Date setBeginDate(String beginDate) throws ParseException {
+    public void setBeginDate(String beginDate) throws ParseException {
         if (beginDate == null || beginDate.isEmpty()) {
-            return null;
+            this.beginDate = null;
+            return;
         }
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        return sdf.parse(beginDate);
+        this.beginDate = sdf.parse(beginDate);
+        System.out.println(beginDate);
     }
 
-    public Date setEndDate(String endDate) throws ParseException {
+    public void setEndDate(String endDate) throws ParseException {
         if (endDate == null || endDate.isEmpty()) {
-            return null;
+            this.endDate = null;
+            return;
         }
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        return sdf.parse(endDate);
+
+        this.endDate = sdf.parse(endDate);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(this.endDate);
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        this.endDate = calendar.getTime();
     }
 
     public String getOperatorName() {
@@ -41,5 +54,14 @@ public class TransactionDTO {
 
     public Date getEndDate() {
         return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionDTO{" +
+                "beginDate=" + beginDate +
+                ", endDate=" + endDate +
+                ", operatorName='" + operatorName + '\'' +
+                '}';
     }
 }
